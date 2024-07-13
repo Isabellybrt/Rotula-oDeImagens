58fa788017b94d0d201579d9cf8b3a0cfeb4030e
Isabellybrt <isabellybrt77@gmail.com>
2024-07-13 16:08:03 -0300

    Projeto Finalizado

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,12 +1,14 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDPzU2WE44jrV8cbplMFsCv1y-5LFeO3qU",
-    authDomain: "uploadimg-fc170.firebaseapp.com",
-    projectId: "uploadimg-fc170",
-    storageBucket: "uploadimg-fc170.appspot.com",
-    messagingSenderId: "779865574756",
-    appId: "1:779865574756:web:64f2e8833bd6a6d50bbe71",
-    measurementId: "G-S2VR1JKHPY"
-    };
+// aqui você precisa colocar o firebaseConfig do seu projeto no console do firebase
+
+// const firebaseConfig = {
+//     apiKey: " ",
+//     authDomain: " ",
+//     projectId: " ",
+//     storageBucket: " ",
+//     messagingSenderId: " ",
+//     appId: " ",
+//     measurementId: " "
+//     };
     
   // Inicializa o Firebase
   firebase.initializeApp(firebaseConfig);
